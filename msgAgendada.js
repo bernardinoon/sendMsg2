15a0c640eca645d64f42edd4687318c21a54c9f2
bernardinoon <142241467+bernardinoon@users.noreply.github.com>
2023-08-18 11:50:46 -0300

    Add files via upload

---
+++ msgAgendada.js
@@ -0,0 +1,61 @@
+const {default: makeWASocket, DisconnectReason,useMultiFileAuthState,sendMessage,MessageType,  
+    MessageOptions, Mimetype } = require("@whiskeysockets/baileys"); 
+const { Boom } = require('@hapi/boom'); 
+//const { msgAgendada } = require('./__server');
+const cron = require('node-cron');
+
+function msgAgendada1() {
+    cron.schedule('*/30 7-9 * * 1-5', () => {
+ 
+async function connectToWhatsApp2() { 
+    const { state, saveCreds } = await useMultiFileAuthState('auth/auth_info_baileys') 
+ 
+    const sock = makeWASocket({ 
+        printQRInTerminal: true, 
+        auth: state, 
+        defaultQueryTimeoutMs: undefined, 
+    }); 
+ 
+    sock.ev.on('connection.update', async (update) => { 
+        const { connection, lastDisconnect } = update; 
+
+
+        if (connection === 'open') {
+            const jid = '5521988642844@s.whatsapp.net' 
+       await sock.sendMessage(jid, { text: 'Mensagem automática para abrir o servidor' })
+   }
+ /*
+        if (connection === 'close') { 
+            const shouldReconnect = (lastDisconnect.error instanceof Boom && 
+                lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut); 
+            console.log('connection closed due to', lastDisconnect.error, ', reconnecting', shouldReconnect); 
+           if (shouldReconnect) { 
+            connectToWhatsApp2(); 
+            } 
+        }
+        
+        */
+    }); 
+
+    
+    sock.ev.on ('creds.update', saveCreds) 
+    
+    
+     
+} 
+connectToWhatsApp2();
+});
+}
+
+function msgAgendada2() {
+    cron.schedule('*/30 13-15 * * 1-5', () => {
+        connectToWhatsApp2();
+    });
+}
+module.exports = {
+    msgAgendada1,
+    msgAgendada2,
+  
+  };
+  
+//msgAgendada()
