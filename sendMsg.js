e11284ccf1c10e7e9700417bb530d981d849fa95
bernardinoon <142241467+bernardinoon@users.noreply.github.com>
2023-08-18 11:52:01 -0300

    Update sendMsg.js

--- sendMsg.js
+++ sendMsg.js
@@ -2,8 +2,9 @@ const { Mimetype } = require("@whiskeysockets/baileys");
 const { connectToWhatsApp } = require('./__server');
 
 
+
 async function wp(sock, number, nome, fluxo,convocacao) {
-  const jid = '55' + number + '@s.whatsapp.net';
+  var jid = '55' + number + '@s.whatsapp.net';
   const aluno = nome;
   const demanda = fluxo;
   var convoc = convocacao;
@@ -18,30 +19,31 @@ var ano = dataAtual.getFullYear();
 var dataFormatada = dia + '/' + mes + '/' + ano;
 
 if(demanda == 'Entrada' || demanda == 'Sáida'){
-  var txt = '*E.M. Bernardino de Melo*' + '\n' +
+  var txt = '                                     *E.M. Bernardino de Melo*' + '\n' +
     'Registro de:' + " " + '*' + demanda + '*' + '\n' +
     'Aluno(a):' + " " + aluno + '\n' +
     'Na data:' + " " + dataFormatada + '\n' + '\n' +
 
-    '*NÃO RESPONDA ESTA MENSAGEM*' + '\n' + '\n' +
-    '_Esta é uma mensagem automática gerada pelo Sistema Escolar Bernardino On_' + '\n' +
+    '                                *NÃO RESPONDA ESTA MENSAGEM*' + '\n' + '\n' +
+    '_Esta é uma mensagem automática gerada pelo Sistema Escolar *Bernardino On*_' + '\n' +
     '_Para maiores informações acesse: www.sigesc.net_' + '\n' +
     '_ou compareça na Secretaria da escola._' + '\n' +
-    '*Juntos somos mais fortes!!!*';
+    '                                     *Juntos somos mais fortes!!!*';
+    
   }else if(demanda == 'Ocorrência'){
 
-    var txt = '*E.M. Bernardino de Melo*' + '\n' +
+    var txt = '                                     *E.M. Bernardino de Melo*' + '\n' +
     'Registro de:' + " " + '*' + demanda + '*' + '\n' +
     'Aluno(a):' + " " + aluno + '\n' +
     'Na data:' + " " + dataFormatada + '\n' + '\n' +
 
     convoc + '\n' + '\n' +
 
-    '*NÃO RESPONDA ESTA MENSAGEM*' + '\n' + '\n' +
-    '_Esta é uma mensagem automática gerada pelo Sistema Escolar Bernardino On_' + '\n' +
+    '                               *NÃO RESPONDA ESTA MENSAGEM*' + '\n' + '\n' +
+    '_Esta é uma mensagem automática gerada pelo Sistema Escolar *Bernardino On*_' + '\n' +
     '_Para maiores informações acesse: www.sigesc.net_' + '\n' +
     '_ou compareça na Secretaria da escola._' + '\n' +
-    '*Juntos somos mais fortes!!!*';
+    '                                     *Juntos somos mais fortes!!!*';
     
   }
 
@@ -50,6 +52,14 @@ if(demanda == 'Entrada' || demanda == 'Sáida'){
   await sock.sendMessage(jid, { text: txt });
 }
 
+
+
+
+
+  
+
+
 module.exports = {
   wp,
+
 };
