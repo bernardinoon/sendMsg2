119c05c9559936eaebc26dba5913a538674bbf12
bernardinoon <142241467+bernardinoon@users.noreply.github.com>
2023-08-19 17:28:51 -0300

    Update msgAgendada.js

--- msgAgendada.js
+++ msgAgendada.js
@@ -3,10 +3,10 @@ const {default: makeWASocket, DisconnectReason,useMultiFileAuthState,sendMessage
 const { Boom } = require('@hapi/boom'); 
 //const { msgAgendada } = require('./__server');
 const cron = require('node-cron');
-//*/30 7-9 * * 1-5
+//    * * * * * 
 
 function msgAgendada1() {
-    cron.schedule('* * * * * ', () => {
+    cron.schedule('*/30 7-9 * * 1-5', () => {
  
 async function connectToWhatsApp2() { 
     const { state, saveCreds } = await useMultiFileAuthState('auth/auth_info_baileys') 
